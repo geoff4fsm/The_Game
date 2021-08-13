6e6c0f2a2087de0c24dd420a9148fae2b6d39b92
geoff4fsm <87156765+geoff4fsm@users.noreply.github.com>
2021-08-13 13:36:54 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Identify Admirals
+while (!player1){
+    var player1 = prompt("Admiral One enter your name. You will be red") ;
+}
+while (!player2){
+    var player2 = prompt("Admiral Two enter your name. You will be blue") ;
+
+}
