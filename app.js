88e1e64c9e0d800d05b6a2c65f777e6c20f7813d
geoff4fsm <87156765+geoff4fsm@users.noreply.github.com>
2021-08-13 23:11:40 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-// Identify Admiral
-while (!player1){
-    var player1 = prompt("Admiral please enter your name") ;
+//  Identify Admiral
+//  while (!player){
+//      var player = prompt("Admiral please enter your name") ;
+//  }
+const userBoard = document.querySelector(".user-board")
+const computerBoard = document.querySelector(".computer-board")
+const gameBoard = document.querySelector(".game-board")
+const ships = document.querySelectorAll(".ship")
+const destroyer = document.querySelector(".destroyer")
+const cruiser = document.querySelector(".cruiser")
+const battleship = document.querySelector(".battleship")
+const startButton = document.querySelector("#start")
+const rotateButton = document.querySelector("#rotate")
+const turnDisplay = document.querySelector("#player-turn")
+const infoDisplay = document.querySelector("#info")
+const width = 8 
+const userSquares = []
+const computerSquares = []
+
+let createBoard = ( board, squares ) => {
+    for ( let i = 0 ; i < width * width ; i++ ) {
+        const square = document.createElement("div")
+        square.dataset.id = i
+        board.appendChild(square)
+        squares.push(square)
+    }
 }
+createBoard( user-board, userSquares )
+createBoard( computer-board, computerSquares)
+
+
 
